2e1bcbf22ad4c30dd73682ff9cb8c7efd717158f
Vladislav Yarmak <vladislav-ex-src@vm-0.com>
2021-05-24 16:11:28 +0300

    fmt and version

--- connpool.go
+++ connpool.go
@@ -1,167 +1,167 @@
 package main
 
 import (
-    "time"
-    "context"
-    "sync"
-    "net"
+	"context"
+	"net"
+	"sync"
+	"time"
 )
 
 type ConnPool struct {
-    size uint
-    ttl, backoff time.Duration
-    connfactory *ConnFactory
-    waiters, prepared *RAQueue
-    qmux sync.Mutex
-    logger *CondLogger
-    ctx context.Context
-    cancel context.CancelFunc
-    shutdown sync.WaitGroup
+	size              uint
+	ttl, backoff      time.Duration
+	connfactory       *ConnFactory
+	waiters, prepared *RAQueue
+	qmux              sync.Mutex
+	logger            *CondLogger
+	ctx               context.Context
+	cancel            context.CancelFunc
+	shutdown          sync.WaitGroup
 }
 
 type watchedConn struct {
-    conn net.Conn
-    cancel context.CancelFunc
-    canceldone chan struct{}
+	conn       net.Conn
+	cancel     context.CancelFunc
+	canceldone chan struct{}
 }
 
 func NewConnPool(size uint, ttl, backoff time.Duration,
-                 connfactory *ConnFactory, logger *CondLogger) *ConnPool {
-    ctx, cancel := context.WithCancel(context.Background())
-    return &ConnPool{
-        size: size,
-        ttl: ttl,
-        backoff: backoff,
-        connfactory: connfactory,
-        waiters: NewRAQueue(),
-        prepared: NewRAQueue(),
-        logger: logger,
-        ctx: ctx,
-        cancel: cancel,
-    }
+	connfactory *ConnFactory, logger *CondLogger) *ConnPool {
+	ctx, cancel := context.WithCancel(context.Background())
+	return &ConnPool{
+		size:        size,
+		ttl:         ttl,
+		backoff:     backoff,
+		connfactory: connfactory,
+		waiters:     NewRAQueue(),
+		prepared:    NewRAQueue(),
+		logger:      logger,
+		ctx:         ctx,
+		cancel:      cancel,
+	}
 }
 
 func (p *ConnPool) Start() {
-    p.shutdown.Add(int(p.size))
-    for i:=uint(0); i<p.size; i++ {
-        go p.worker()
-    }
+	p.shutdown.Add(int(p.size))
+	for i := uint(0); i < p.size; i++ {
+		go p.worker()
+	}
 }
 
 func (p *ConnPool) do_backoff() {
-    select {
-    case <-AfterWallClock(p.backoff):
-    case <-p.ctx.Done():
-    }
+	select {
+	case <-AfterWallClock(p.backoff):
+	case <-p.ctx.Done():
+	}
 }
 
 func (p *ConnPool) kill_prepared(queue_id uint, watched *watchedConn, output_ch chan *watchedConn) {
-    p.qmux.Lock()
-    deleted_elem := p.prepared.Delete(queue_id)
-    p.qmux.Unlock()
-    if deleted_elem == nil {
-        // Someone already grabbed this slot from queue. Dispatch anyway.
-        p.logger.Debug("Dead conn %v was grabbed from queue", watched.conn.LocalAddr())
-        output_ch <-watched
-    } else {
-        watched.conn.Close()
-    }
+	p.qmux.Lock()
+	deleted_elem := p.prepared.Delete(queue_id)
+	p.qmux.Unlock()
+	if deleted_elem == nil {
+		// Someone already grabbed this slot from queue. Dispatch anyway.
+		p.logger.Debug("Dead conn %v was grabbed from queue", watched.conn.LocalAddr())
+		output_ch <- watched
+	} else {
+		watched.conn.Close()
+	}
 }
 
 func (p *ConnPool) worker() {
-    defer p.shutdown.Done()
-    output_ch := make(chan *watchedConn)
-    dummybuf := make([]byte, 1)
-    for {
-        select {
-        case <-p.ctx.Done():
-            return
-        default:
-        }
-        conn, err := p.connfactory.DialContext(p.ctx)
-        if err != nil {
-            select {
-            case <-p.ctx.Done():
-                return
-            default:
-                p.logger.Error("Upstream connection error: %s", err)
-                p.do_backoff()
-                continue
-            }
-        }
-        localaddr := conn.LocalAddr()
-        p.logger.Debug("Established upstream connection %v", localaddr)
+	defer p.shutdown.Done()
+	output_ch := make(chan *watchedConn)
+	dummybuf := make([]byte, 1)
+	for {
+		select {
+		case <-p.ctx.Done():
+			return
+		default:
+		}
+		conn, err := p.connfactory.DialContext(p.ctx)
+		if err != nil {
+			select {
+			case <-p.ctx.Done():
+				return
+			default:
+				p.logger.Error("Upstream connection error: %s", err)
+				p.do_backoff()
+				continue
+			}
+		}
+		localaddr := conn.LocalAddr()
+		p.logger.Debug("Established upstream connection %v", localaddr)
 
-        p.qmux.Lock()
-        waiter := p.waiters.Pop()
-        if waiter != nil {
-            p.qmux.Unlock()
-            waiter.(chan net.Conn) <-conn
-            p.logger.Warning("Pool connection delivered directly to waiter")
-        } else {
-            queue_id := p.prepared.Push(output_ch)
-            p.qmux.Unlock()
-            readctx, readcancel := context.WithCancel(p.ctx)
-            readdone := make(chan struct{}, 1)
-            go func() {
-                conn.ReadContext(readctx, dummybuf)
-                readdone <-struct{}{}
-            }()
-            watched := &watchedConn{conn, readcancel, readdone}
-            select {
-            // Connection delivered via queue
-            case output_ch <-watched:
-                p.logger.Debug("Pool connection %v delivered via queue", localaddr)
-            // Connection disrupted
-            case <-readdone:
-                p.logger.Debug("Pool connection %v was disrupted", localaddr)
-                p.kill_prepared(queue_id, watched, output_ch)
-                p.do_backoff()
-            // Expired
-            case <-AfterWallClock(p.ttl):
-                p.logger.Debug("Connection %v seem to be expired", localaddr)
-                p.kill_prepared(queue_id, watched, output_ch)
-            // Pool context cancelled
-            case <-p.ctx.Done():
-                conn.Close()
-            }
-        }
-    }
+		p.qmux.Lock()
+		waiter := p.waiters.Pop()
+		if waiter != nil {
+			p.qmux.Unlock()
+			waiter.(chan net.Conn) <- conn
+			p.logger.Warning("Pool connection delivered directly to waiter")
+		} else {
+			queue_id := p.prepared.Push(output_ch)
+			p.qmux.Unlock()
+			readctx, readcancel := context.WithCancel(p.ctx)
+			readdone := make(chan struct{}, 1)
+			go func() {
+				conn.ReadContext(readctx, dummybuf)
+				readdone <- struct{}{}
+			}()
+			watched := &watchedConn{conn, readcancel, readdone}
+			select {
+			// Connection delivered via queue
+			case output_ch <- watched:
+				p.logger.Debug("Pool connection %v delivered via queue", localaddr)
+			// Connection disrupted
+			case <-readdone:
+				p.logger.Debug("Pool connection %v was disrupted", localaddr)
+				p.kill_prepared(queue_id, watched, output_ch)
+				p.do_backoff()
+			// Expired
+			case <-AfterWallClock(p.ttl):
+				p.logger.Debug("Connection %v seem to be expired", localaddr)
+				p.kill_prepared(queue_id, watched, output_ch)
+			// Pool context cancelled
+			case <-p.ctx.Done():
+				conn.Close()
+			}
+		}
+	}
 }
 
 func (p *ConnPool) Get(ctx context.Context) chan net.Conn {
-    out := make(chan net.Conn, 1)
-    p.qmux.Lock()
-    free := p.prepared.Pop()
-    if free == nil {
-        waiter_ch := make(chan net.Conn, 1)
-        queue_id := p.waiters.Push(waiter_ch)
-        p.qmux.Unlock()
-        go func () {
-            select {
-            case <-ctx.Done():
-                out <-nil
-                // Try to remove conn request from waiter queue
-                p.qmux.Lock()
-                p.waiters.Delete(queue_id)
-                p.qmux.Unlock()
-            case <-p.ctx.Done():
-                out <-nil
-            case conn := <-waiter_ch:
-                out <-conn
-            }
-        }()
-    } else {
-        p.qmux.Unlock()
-        watched := <-(free.(chan *watchedConn))
-        watched.cancel()
-        <-watched.canceldone
-        out <-watched.conn
-    }
-    return out
+	out := make(chan net.Conn, 1)
+	p.qmux.Lock()
+	free := p.prepared.Pop()
+	if free == nil {
+		waiter_ch := make(chan net.Conn, 1)
+		queue_id := p.waiters.Push(waiter_ch)
+		p.qmux.Unlock()
+		go func() {
+			select {
+			case <-ctx.Done():
+				out <- nil
+				// Try to remove conn request from waiter queue
+				p.qmux.Lock()
+				p.waiters.Delete(queue_id)
+				p.qmux.Unlock()
+			case <-p.ctx.Done():
+				out <- nil
+			case conn := <-waiter_ch:
+				out <- conn
+			}
+		}()
+	} else {
+		p.qmux.Unlock()
+		watched := <-(free.(chan *watchedConn))
+		watched.cancel()
+		<-watched.canceldone
+		out <- watched.conn
+	}
+	return out
 }
 
 func (p *ConnPool) Stop() {
-    p.cancel()
-    p.shutdown.Wait()
+	p.cancel()
+	p.shutdown.Wait()
 }
--- listener.go
+++ listener.go
@@ -1,82 +1,82 @@
 package main
 
 import (
+	"context"
 	"net"
-    "context"
-    "sync"
+	"sync"
 )
 
 type HandlerFunc func(context.Context, net.Conn)
 
 type TCPListener struct {
-    address string
-    port uint16
-    handler HandlerFunc
-    quitaccept chan struct{}
-    listener *net.TCPListener
-    logger *CondLogger
-    ctx context.Context
-    cancel context.CancelFunc
-    shutdown sync.WaitGroup
+	address    string
+	port       uint16
+	handler    HandlerFunc
+	quitaccept chan struct{}
+	listener   *net.TCPListener
+	logger     *CondLogger
+	ctx        context.Context
+	cancel     context.CancelFunc
+	shutdown   sync.WaitGroup
 }
 
 func NewTCPListener(address string, port uint16, handler HandlerFunc,
-                    logger *CondLogger) *TCPListener {
-    ctx, cancel := context.WithCancel(context.Background())
-    return &TCPListener{
-        address: address,
-        port: port,
-        handler: handler,
-        logger: logger,
-        quitaccept: make(chan struct{}, 1),
-        ctx: ctx,
-        cancel: cancel,
-    }
+	logger *CondLogger) *TCPListener {
+	ctx, cancel := context.WithCancel(context.Background())
+	return &TCPListener{
+		address:    address,
+		port:       port,
+		handler:    handler,
+		logger:     logger,
+		quitaccept: make(chan struct{}, 1),
+		ctx:        ctx,
+		cancel:     cancel,
+	}
 }
 
 func (l *TCPListener) Start() error {
-    ips, err := net.LookupIP(l.address)
-    if err != nil {
-        return err
-    }
-    listener, err := net.ListenTCP("tcp", &net.TCPAddr{
-        IP: ips[0],
-        Port: int(l.port),
-    })
-    if err != nil {
-        return err
-    }
-    l.listener = listener
-    go l.serve()
-    return nil
+	ips, err := net.LookupIP(l.address)
+	if err != nil {
+		return err
+	}
+	listener, err := net.ListenTCP("tcp", &net.TCPAddr{
+		IP:   ips[0],
+		Port: int(l.port),
+	})
+	if err != nil {
+		return err
+	}
+	l.listener = listener
+	go l.serve()
+	return nil
 }
 
 func (l *TCPListener) serve() {
-    for {
-        conn, err := l.listener.Accept()
-        if err != nil {
-            select {
-            case <-l.quitaccept:
-                l.logger.Info("Leaving accept loop.")
-                l.quitaccept <- struct{}{}
-                return
-            default:
-                l.logger.Error("Accept error: %s", err)
-                continue
-            }
-        }
-        l.shutdown.Add(1)
-        go func(c net.Conn) {
-            defer l.shutdown.Done()
-            l.handler(l.ctx, c)
-        }(conn)
-    }
+	for {
+		conn, err := l.listener.Accept()
+		if err != nil {
+			select {
+			case <-l.quitaccept:
+				l.logger.Info("Leaving accept loop.")
+				l.quitaccept <- struct{}{}
+				return
+			default:
+				l.logger.Error("Accept error: %s", err)
+				continue
+			}
+		}
+		l.shutdown.Add(1)
+		go func(c net.Conn) {
+			defer l.shutdown.Done()
+			l.handler(l.ctx, c)
+		}(conn)
+	}
 }
 
 func (l *TCPListener) Stop() {
-    l.quitaccept <- struct{}{}
-    l.listener.Close()
-    <-l.quitaccept
-    l.cancel()
-    l.shutdown.Wait()
+	l.quitaccept <- struct{}{}
+	l.listener.Close()
+	<-l.quitaccept
+	l.cancel()
+	l.shutdown.Wait()
 }
